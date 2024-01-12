8ff9aba72f201fb3ab8f9b0aa7dda8600023d245
adeolaemmanuel006@gmail.com <adeolaemmanuel006@gmail.com>
2024-01-12 23:49:54 +0100

    update on package.json script

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,13 +1,13 @@
 let {
-  REFRESH_TOKEN_LENGTH,
-  DEFAULT_RANDOM,
-  DB_USERNAME,
-  DB_NAME,
-  DB_HOST,
-  DB_PORT,
-  DB_PASSWORD,
-  DB_URL,
-  PORT,
+	REFRESH_TOKEN_LENGTH,
+	DEFAULT_RANDOM,
+	DB_USERNAME,
+	DB_NAME,
+	DB_HOST,
+	DB_PORT,
+	DB_PASSWORD,
+	DB_URL,
+	PORT,
 } = process.env;
 
 DB_NAME = DB_NAME || "postgres";
@@ -16,15 +16,18 @@ DB_USERNAME = DB_USERNAME || "postgres";
 DB_PASSWORD = DB_PASSWORD || "neutron@360";
 DB_PORT = DB_PORT || "5432";
 PORT = PORT || "4000";
+DB_URL =
+	DB_URL ||
+	"postgresql://Adeolaemmanuel:5n0vqjxGgyVQ@ep-long-cloud-a5e8xrjk.us-east-2.aws.neon.tech/aers_dev?sslmode=require";
 
 export {
-  REFRESH_TOKEN_LENGTH,
-  DEFAULT_RANDOM,
-  DB_USERNAME,
-  DB_NAME,
-  DB_HOST,
-  DB_PORT,
-  DB_PASSWORD,
-  DB_URL,
-  PORT
+	REFRESH_TOKEN_LENGTH,
+	DEFAULT_RANDOM,
+	DB_USERNAME,
+	DB_NAME,
+	DB_HOST,
+	DB_PORT,
+	DB_PASSWORD,
+	DB_URL,
+	PORT,
 };
