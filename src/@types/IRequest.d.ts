524caff991a9272c730cfee34124d01e0f237743
adeolaemmanuel006@gmail.com <adeolaemmanuel006@gmail.com>
2024-02-08 22:13:46 +0100

    update on vercel again

--- src/@types/IRequest.d.ts
+++ src/@types/IRequest.d.ts
@@ -2,7 +2,7 @@ import { Request } from "express";
 import AdminUsers from "modules/admin/entities/admin.entity";
 import Users from "modules/user/entities/user.entity";
 
-interface IRequest extends Request {
+export interface IRequest extends Request {
 	user: Users;
 	admin: AdminUsers;
 }
