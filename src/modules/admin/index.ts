7171a83816ce1b469e4fd1f470f57dd45ff851be
adeolaemmanuel006@gmail.com <adeolaemmanuel006@gmail.com>
2024-03-26 00:56:12 +0100

    update

--- src/modules/admin/index.ts
+++ src/modules/admin/index.ts
@@ -3,6 +3,6 @@ import AdminLoginController from "./controller/v1.controller";
 
 const adminRouterV1 = express.Router();
 
-adminRouterV1.post("login", AdminLoginController);
+adminRouterV1.post("/login", AdminLoginController);
 
 export default adminRouterV1
